302b67a557244f8948b5a6f1dd7a6e058e54d928
William <william.jaimesfontalvo@gmail.com>
2026-08-11 12:43:49 -0500

    test: add GetAllAsync test for empty Name

--- JobTracker.Api.Tests/Services/CompanyServiceTests.cs
+++ JobTracker.Api.Tests/Services/CompanyServiceTests.cs
@@ -179,6 +179,30 @@ public class CompanyServiceTests
     }
 
     [Fact]
+    public async Task GetAllAsync_WhenNameDoesNotExist_ReturnsEmptyPagedResponse()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<ApiDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        
+        using var context = new ApiDbContext(options);
+
+        await SeedDatabaseAsync(context);
+
+        var service = new CompanyService(context);
+        var searchDto = new CompanySearchDto{ Name = "Naughty Dog" };
+
+        // Act
+        var result = await service.GetAllAsync(searchDto);
+
+        // Assert
+        Assert.Empty(result.Items);
+        Assert.Equal(0, result.TotalPages);
+        Assert.Equal(0, result.TotalRecords);
+    }
+
+    [Fact]
     public async Task GetAllAsync_WhenRecordsIsEighty_ReturnsRecordsIsFifty()
     {
         // Arrange

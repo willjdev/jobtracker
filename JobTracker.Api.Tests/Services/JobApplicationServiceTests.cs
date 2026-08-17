0b5d920b7d01e48e4998f05fd8e961cf7f444231
William <william.jaimesfontalvo@gmail.com>
2026-08-17 12:51:59 -0500

    feat: create JobApplicationServiceTests and seed data helper

---
+++ JobTracker.Api.Tests/Services/JobApplicationServiceTests.cs
@@ -0,0 +1,122 @@
+using Microsoft.EntityFrameworkCore;
+using JobTracker.Api.Services;
+using JobTracker.Api.Data;
+using JobTracker.Api.Models;
+using JobTracker.Api.Dtos.JobApplicationDto;
+
+namespace JobTracker.Api.Tests.Services;
+
+public class JobApplicationServiceTests
+{
+    private async Task SeedDatabaseAsync(ApiDbContext context)
+    {
+        var jobApplications = new List<JobApplication>
+        {
+            new()
+            {
+                Id = 1,
+                Position = "Fullstack Developer",
+                JobUrl = "https://www.job.com",
+                AppliedAt = new DateTime(2026, 8, 17, 6, 20, 0),
+                CompanyId = 1,
+                Company = new Company
+                {
+                    Id = 1,
+                    Name = "Microsoft",
+                    Description = "Big Company",
+                    Website = "www.microsoft.com",
+                    Location = "Holand",
+                    CreatedAt = new DateTime(2026, 7, 10, 6, 10, 0),
+                },
+                ApplicationNotes = new List<ApplicationNote>
+                {
+                    new()
+                    {
+                        Id = 1,
+                        Content = "Waiting response",
+                        CreatedAt = new DateTime(2026, 8, 17, 6, 30, 0),
+                        JobApplicationId = 1
+                    },
+                    new()
+                    {
+                        Id = 2,
+                        Content = "Resume sent",
+                        CreatedAt = new DateTime(2026, 8, 18, 7, 30, 0),
+                        JobApplicationId = 1
+                    }
+                }
+            },
+            new()
+            {
+               Id = 2,
+               Position = "Game Developer",
+               AppliedAt = new DateTime(2026, 8, 18, 7, 40, 0),
+               JobUrl = "https//www.job.com",
+               CompanyId = 2,
+               Company = new Company
+               {
+                    Id = 2,
+                    Name = "Santa Monica",
+                    Description = "Game Company",
+                    Website = "www.santamonica.com",
+                    Location = "Remote",
+                    CreatedAt = new DateTime(2026, 7, 20, 7, 0, 0),
+               },
+               ApplicationNotes = new List<ApplicationNote>
+               {
+                   new()
+                   {
+                        Id = 3,
+                        Content = "Sent application, waiting for response",
+                        CreatedAt = new DateTime(2026, 8, 18, 8, 20, 0),
+                        JobApplicationId = 2
+                   },
+                   new()
+                   {
+                       Id = 4,
+                       Content = "Received email!!",
+                       CreatedAt = new DateTime(2026, 8, 19, 8, 24, 0),
+                       JobApplicationId = 2
+                   }
+               }
+            },
+            new()
+            {
+                Id = 3,
+                Position = ".NET Developer",
+                AppliedAt = new DateTime(2026, 8, 20, 6, 40, 0),
+                JobUrl = "https//www.job.com",
+                CompanyId = 3,
+                Company = new Company
+                {
+                    Id = 3,
+                    Name = "Microsoft Colombia",
+                    Description = "Big Company",
+                    Website = "www.microsoft.com",
+                    Location = "Colombia",
+                    CreatedAt = new DateTime(2026, 8, 2, 8, 20, 0)
+                },
+                ApplicationNotes = new List<ApplicationNote>
+                {
+                    new()
+                    {
+                        Id = 5,
+                        Content = "Sent an email to recruiter",
+                        CreatedAt = new DateTime(2026, 8, 20, 9, 0, 0),
+                        JobApplicationId = 3
+                    },
+                    new()
+                    {
+                        Id = 6,
+                        Content = "Recruiter wants to interview me!",
+                        CreatedAt = new DateTime(2026, 8, 22, 10, 0, 0),
+                        JobApplicationId = 3
+                    }
+                }
+            }
+        };
+
+        await context.Applications.AddRangeAsync(jobApplications);
+        await context.SaveChangesAsync();
+    }
+}

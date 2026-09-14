6c4edb5a872b717a5fc7e4835a2f55e3233aa55f
William <william.jaimesfontalvo@gmail.com>
2026-09-14 06:56:13 -0500

    feat: add Identity package and ApplicationUser model

---
+++ JobTracker.api/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required(ErrorMessage = "Name is required")]
+    [MinLength(1)]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Lastname is required")]
+    [MinLength(1)]
+    public string Lastname { get; set; }
+}

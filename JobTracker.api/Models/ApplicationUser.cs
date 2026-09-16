af32bccf0ffca9daac8256460c5b8ed98864db40
William <william.jaimesfontalvo@gmail.com>
2026-09-16 05:39:43 -0500

    refactor(ApplicatonUser): initialize ApplicationUser string properties with string.Empty

--- JobTracker.api/Models/ApplicationUser.cs
+++ JobTracker.api/Models/ApplicationUser.cs
@@ -6,11 +6,11 @@ public class ApplicationUser : IdentityUser
 {
     [Required(ErrorMessage = "Name is required")]
     [MinLength(1)]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Lastname is required")]
     [MinLength(1)]
-    public string Lastname { get; set; }
+    public string Lastname { get; set; } = string.Empty;
 
     public DateTime CreateAt { get; set; } = DateTime.UtcNow;
 }

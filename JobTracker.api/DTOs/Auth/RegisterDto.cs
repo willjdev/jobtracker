d118b9ad620c9525fe42339d0ff5397c5e75d635
William <william.jaimesfontalvo@gmail.com>
2026-09-16 05:37:20 -0500

    feat: complete RegisterDto

--- JobTracker.api/DTOs/Auth/RegisterDto.cs
+++ JobTracker.api/DTOs/Auth/RegisterDto.cs
@@ -6,9 +6,17 @@ public class RegisterDto
 {
     [Required(ErrorMessage = "Name is required")]
     [MinLength(1)]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Lastname is required")]
     [MinLength(1)]
-    public string Lastname { get; set; }
+    public string Lastname { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
 }

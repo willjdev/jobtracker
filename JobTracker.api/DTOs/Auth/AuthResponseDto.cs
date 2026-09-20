2696970436b3a20488790b025bd3300114e06b0c
William <william.jaimesfontalvo@gmail.com>
2026-09-20 11:35:14 -0500

    refactor(auth): expand AuthRespondeDto

--- JobTracker.api/DTOs/Auth/AuthResponseDto.cs
+++ JobTracker.api/DTOs/Auth/AuthResponseDto.cs
@@ -8,4 +8,6 @@ public class AuthResponseDto
     public string Message { get; set; } = string.Empty;
     public string? Token { get; set; }
     public DateTime? Expiration { get; set; }
+    public string? UserId { get; set; }
+    public string? Email { get; set; }
 }

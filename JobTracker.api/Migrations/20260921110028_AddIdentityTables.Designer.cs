1f467b2d759abffcc1d9700adfad278499703054
William <william.jaimesfontalvo@gmail.com>
2026-09-21 06:02:43 -0500

    feat(auth): update Identity tables with migration

--- JobTracker.api/Migrations/20260921110028_AddIdentityTables.Designer.cs
+++ JobTracker.api/Migrations/20260921110028_AddIdentityTables.Designer.cs
@@ -4,6 +4,7 @@ using JobTracker.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobTracker.api.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260921110028_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JobTracker.api/Migrations/20260921110028_AddIdentityTables.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracker.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "AspNetUsers",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                newName: "CreateAt");
+        }
+    }
+}

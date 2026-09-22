88b9b0fd17ca4d376f0201d51c6dfd180c59d3a0
William <william.jaimesfontalvo@gmail.com>
2026-09-22 05:52:25 -0500

    fix(auth): add getters and setters to LoginDto to fix model binding

--- JobTracker.api/DTOs/Auth/LoginDto.cs
+++ JobTracker.api/DTOs/Auth/LoginDto.cs
@@ -11,5 +11,5 @@ public class LoginDto
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
-    public string Password = string.Empty;
+    public string Password { get; set;} = string.Empty;
 }
